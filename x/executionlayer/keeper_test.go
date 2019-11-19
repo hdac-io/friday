e94ca32babbf9e8c92aba4dd8ca595fce33a93d6
Bryan <psy2848048@gmail.com>
2019-11-20 07:24:09 +0900

    Keeper test code written

--- x/executionlayer/keeper_test.go
+++ x/executionlayer/keeper_test.go
@@ -1,1 +1,39 @@
 package executionlayer
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	sdk "github.com/hdac-io/friday/types"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//-------------------------------------------
+
+func TestGetQueryResult(t *testing.T) {
+	input := setupTestInput()
+	path := "counter/count"
+
+	keeper := NewExecutionLayerKeeper(input.cdc, sdk.NewKVStoreKey(HashMapStoreKey), sdk.NewKVStoreKey(DeployStoreKey),
+		os.ExpandEnv("$HOME/.casperlabs/.casper-node.sock"), "1.0.0")
+
+	parentHash := genesis(keeper)
+	parentHash = counterDefine(keeper, parentHash)
+	parentHash = counterCall(keeper, parentHash)
+
+	res, err := keeper.GetQueryResult(
+		input.ctx,
+		parentHash,
+		"address", input.genesisAddress, path)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Fail to execute")
+	}
+
+	fmt.Println(res)
+
+	assert.NotNil(t, res)
+}
