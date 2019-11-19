8b9e6179bb0772e9edd863e9863f255d7bdcd90a
Yun <iam.yunjoowon@gmail.com>
2019-11-19 19:00:54 +0900

    Initial keeper

--- x/executionlayer/querier.go
+++ x/executionlayer/querier.go
@@ -1,1 +1,17 @@
 package executionlayer
+
+import (
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	sdk "github.com/hdac-io/friday/types"
+)
+
+// creates a querier for auth REST endpoints
+func NewQuerier(keeper ExecutionLayerKeeper) sdk.Querier {
+	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, sdk.Error) {
+		switch path[0] {
+		default:
+			return nil, sdk.ErrUnknownRequest("unknown auth query endpoint")
+		}
+	}
+}
---
+++ x/executionlayer/test_common.go
@@ -0,0 +1,34 @@
+package executionlayer
+
+import (
+	"github.com/hdac-io/friday/codec"
+	"github.com/hdac-io/friday/store"
+	sdk "github.com/hdac-io/friday/types"
+	"github.com/hdac-io/friday/x/executionlayer/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+)
+
+type testInput struct {
+	cdc *codec.Codec
+	ctx sdk.Context
+}
+
+func setupTestInput() testInput {
+	db := dbm.NewMemDB()
+
+	cdc := codec.New()
+	types.RegisterCodec(cdc)
+	codec.RegisterCrypto(cdc)
+
+	unitHashMapKey := sdk.NewKVStoreKey("unitHashMapKey")
+
+	ms := store.NewCommitMultiStore(db)
+	ms.MountStoreWithDB(unitHashMapKey, sdk.StoreTypeIAVL, db)
+	ms.LoadLatestVersion()
+
+	ctx := sdk.NewContext(ms, abci.Header{ChainID: "test-chain-id"}, false, log.NewNopLogger())
+
+	return testInput{cdc: cdc, ctx: ctx}
+}
