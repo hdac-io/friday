b311de98675c111bff26a3dffa0cfa6fc22e7176
Yun <iam.yunjoowon@gmail.com>
2019-12-02 18:11:40 +0900

    feat: add to bond and unbond cli

--- x/executionlayer/client/cli/tx.go
+++ x/executionlayer/client/cli/tx.go
@@ -27,7 +27,7 @@ func GetExecutionLayerTxCmd(cdc *codec.Codec) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 	executionlayerTxCmd.AddCommand(client.GetCommands(
-		GetCmdTransfer(cdc),
+		GetCmdTransfer(cdc), GetCmdBonding(cdc), GetCmdUnbonding(cdc),
 	)...)
 	return executionlayerTxCmd
 }
@@ -73,6 +73,82 @@ func GetCmdTransfer(cdc *codec.Codec) *cobra.Command {
 
 			// build and sign the transaction, then broadcast to Tendermint
 			msg := types.NewMsgExecute([]byte{0}, cliCtx.FromAddress, cliCtx.FromAddress, transferCode, transferAbi, paymentCode, paymentAbi, gasPrice)
+			txBldr = txBldr.WithGas(gasPrice)
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
+}
+
+// GetCmdBonding is the CLI command for bonding
+func GetCmdBonding(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "bond [from_key_or_address] [amount] [fee] [gas_price]",
+		Short: "Create and sign a bonding tx",
+		Args:  cobra.ExactArgs(4), // # of arguments
+		RunE: func(cmd *cobra.Command, args []string) error {
+			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+			cliCtx := context.NewCLIContextWithFrom(args[0]).WithCodec(cdc)
+
+			coins, err := strconv.ParseUint(args[1], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			fee, err := strconv.ParseUint(args[2], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			gasPrice, err := strconv.ParseUint(args[3], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			bondingCode := util.LoadWasmFile(os.ExpandEnv("$HOME/.nodef/contracts/bonding.wasm"))
+			bondingAbi := util.MakeArgsBonding(coins)
+			paymentCode := util.LoadWasmFile(os.ExpandEnv("$HOME/.nodef/contracts/standard_payment.wasm"))
+			paymentAbi := util.MakeArgsStandardPayment(new(big.Int).SetUint64(fee))
+
+			// build and sign the transaction, then broadcast to Tendermint
+			msg := types.NewMsgExecute([]byte{0}, cliCtx.FromAddress, cliCtx.FromAddress, bondingCode, bondingAbi, paymentCode, paymentAbi, gasPrice)
+			txBldr = txBldr.WithGas(gasPrice)
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
+}
+
+// GetCmdUnbonding is the CLI command for unbonding
+func GetCmdUnbonding(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "unbond [from_key_or_address] [amount] [fee] [gas_price]",
+		Short: "Create and sign a unbonding tx",
+		Args:  cobra.ExactArgs(4), // # of arguments
+		RunE: func(cmd *cobra.Command, args []string) error {
+			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+			cliCtx := context.NewCLIContextWithFrom(args[0]).WithCodec(cdc)
+
+			coins, err := strconv.ParseUint(args[1], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			fee, err := strconv.ParseUint(args[2], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			gasPrice, err := strconv.ParseUint(args[3], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			unbondingCode := util.LoadWasmFile(os.ExpandEnv("$HOME/.nodef/contracts/unbonding.wasm"))
+			unbondingAbi := util.MakeArgsBonding(coins)
+			paymentCode := util.LoadWasmFile(os.ExpandEnv("$HOME/.nodef/contracts/standard_payment.wasm"))
+			paymentAbi := util.MakeArgsStandardPayment(new(big.Int).SetUint64(fee))
+
+			// build and sign the transaction, then broadcast to Tendermint
+			msg := types.NewMsgExecute([]byte{0}, cliCtx.FromAddress, cliCtx.FromAddress, unbondingCode, unbondingAbi, paymentCode, paymentAbi, gasPrice)
 			txBldr = txBldr.WithGas(gasPrice)
 			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
 		},
