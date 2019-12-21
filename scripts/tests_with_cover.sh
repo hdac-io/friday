434603a8080e366971bf755deaf0b756e69d2b7d
Bryan <psy2848048@gmail.com>
2019-12-21 23:34:48 +0900

    fix: omit cmd, rest, cli tests from go test iteration

--- scripts/tests_with_cover.sh
+++ scripts/tests_with_cover.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 echo "" > coverage.txt
-for d in $(go list ./...); do
+for d in $(go list ./... | grep -v "client/cli" | grep -v "client/rest" | grep -v "friday/cmd"); do
   go test -v -race -coverprofile=profile.out $d
   if [ -f profile.out ]; then
-    cat profile.out | grep -v "client/cli" | grep -v "client/rest" | grep -v "friday/cmd" >> coverage.txt
+    cat profile.out >> coverage.txt
     rm profile.out
   fi
 done
