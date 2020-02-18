f24bd7a5bdb6571bc578394faae0daf7bb313a35
Bryan <psy2848048@gmail.com>
2020-02-19 08:35:04 +0900

    feat: applied the rollbacked "--from" in transfer

--- x/executionlayer/client/util/util.go
+++ x/executionlayer/client/util/util.go
@@ -8,6 +8,7 @@ import (
 	"github.com/hdac-io/casperlabs-ee-grpc-go-util/util"
 	"github.com/hdac-io/friday/client/context"
 	"github.com/hdac-io/friday/codec"
+	"github.com/hdac-io/friday/crypto/keys"
 	sdk "github.com/hdac-io/friday/types"
 	idtype "github.com/hdac-io/friday/x/nickname/types"
 )
@@ -63,4 +64,47 @@ func ProtobufSafeDecodeingHexString(str string) ([]byte, error) {
 	}
 
 	return res, nil
+}
+
+// GetLocalWalletInfo takes wallet info from local
+// Rules:
+// 1) If --from exists, search according to:
+//  (1) Local wallet alias
+//  (2) Address
+//  (3) Nickname
+// 2) If --from doesn't exist,
+//  (1) If only one wallet exists, take this
+//  (2) If not, raise error
+func GetLocalWalletInfo(valueFromFromFlag string, kb keys.Keybase, cdc *codec.Codec, cliCtx context.CLIContext) (keys.Info, error) {
+	if valueFromFromFlag != "" {
+		// Find in local wallet
+		key, err := kb.Get(valueFromFromFlag)
+		if err == nil {
+			return key, nil
+		}
+
+		// If not exist, try parsing into address and find in nickname
+		addr, err := GetAddress(cdc, cliCtx, valueFromFromFlag)
+		if err != nil {
+			return nil, fmt.Errorf("cannot parse into address, or no registered address of the given nickname '%s'", valueFromFromFlag)
+		}
+		key, err = kb.GetByAddress(addr)
+		if err != nil {
+			return nil, err
+		}
+		return key, nil
+	}
+
+	infoList, err := kb.List()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(infoList) > 1 {
+		return nil, fmt.Errorf("multiple wallets in local. Cannot specify one wallet")
+	} else if len(infoList) == 0 {
+		return nil, fmt.Errorf("no wallet in local")
+	}
+
+	return infoList[0], nil
 }
