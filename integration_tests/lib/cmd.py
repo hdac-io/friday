0a52daf8d2f64e6e711890e05f132ff0195750fa
Bryan <psy2848048@gmail.com>
2020-02-21 17:09:16 +0900

    test: change client home to ".test_clif"

--- integration_tests/lib/cmd.py
+++ integration_tests/lib/cmd.py
@@ -17,7 +17,12 @@ def _process_executor(cmd: str, *args, need_output=False):
     outs = child.read().decode('utf-8')
 
     if need_output == True:
-        res = json.loads(outs)
+        try:
+            res = json.loads(outs)
+        except Exception as e:
+            print(e)
+            raise e
+
         return res
 
 
@@ -30,7 +35,11 @@ def _tx_executor(cmd: str, passphrase, *args):
         _ = child.sendline(passphrase)
         
         outs = child.read().decode('utf-8')
-        tx_hash = re.search(r'"txhash": "([A-Z0-9]+)"', outs).group(1)
+        try:
+            tx_hash = re.search(r'"txhash": "([A-Z0-9]+)"', outs).group(1)
+        except Exception as e:
+            print(outs)
+            raise e
 
     except pexpect.TIMEOUT:
         raise FinishedWithError
@@ -86,12 +95,13 @@ def copy_manifest():
     _ = _process_executor(cmd, need_output=False)
 
 
-def create_wallet(wallet_alias: str, passphrase: str):
+def create_wallet(wallet_alias: str, passphrase: str, client_home: str = '.test_clif'):
     """
     clif key add <wallet_alias>
     """
+    client_home = os.path.join(os.environ["HOME"], client_home)
     try:
-        child = pexpect.spawn("clif keys add {}".format(wallet_alias))
+        child = pexpect.spawn("clif keys add {} --home {}".format(wallet_alias, client_home))
         _ = child.read_nonblocking(10000, timeout=1)
         _ = child.sendline(passphrase)
         _ = child.read_nonblocking(10000, timeout=1)
@@ -114,14 +124,18 @@ def create_wallet(wallet_alias: str, passphrase: str):
         mnemonic = res['mnemonic']
 
     except json.JSONDecodeError:
-        # If output is not json
-        address = re.search(r"address: ([a-z0-9]+)", outs).group(1)
-        pubkey = re.search(r"pubkey: ([a-z0-9]+)", outs).group(1)
-        mnemonic = outs.strip().split('\n')[-1]
+        try:
+            # If output is not json
+            address = re.search(r"address: ([a-z0-9]+)", outs).group(1)
+            pubkey = re.search(r"pubkey: ([a-z0-9]+)", outs).group(1)
+            mnemonic = outs.strip().split('\n')[-1]
+        except Exception as e:
+            print(outs)
+            raise e
 
     except Exception as e:
-        print(e)
-        return
+        print(outs)
+        raise e
 
     res = {
         "address": address,
@@ -132,20 +146,22 @@ def create_wallet(wallet_alias: str, passphrase: str):
     return res
 
 
-def get_wallet_info(wallet_alias: str):
+def get_wallet_info(wallet_alias: str, client_home: str = '.test_clif'):
     """
     clif keys show <wallet_alias>
     """
-    res = _process_executor("clif keys show {}", wallet_alias, need_output=True)
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    res = _process_executor("clif keys show {} --home {}", wallet_alias, client_home, need_output=True)
     return res
 
 
-def delete_wallet(wallet_alias: str, passphrase: str):
+def delete_wallet(wallet_alias: str, passphrase: str, client_home: str = '.test_clif'):
     """
     clif key delete <wallet_alias>
     """
+    client_home = os.path.join(os.environ["HOME"], client_home)
     try:
-        child = pexpect.spawn("clif keys delete {}".format(wallet_alias))
+        child = pexpect.spawn("clif keys delete {} --home {}".format(wallet_alias, client_home))
         _ = child.read_nonblocking(10000, timeout=1)
         _ = child.sendline(passphrase)
         
@@ -155,32 +171,35 @@ def delete_wallet(wallet_alias: str, passphrase: str):
         raise FinishedWithError
 
 
-def add_genesis_account(address: str, coin: int, stake: int):
+def add_genesis_account(address: str, coin: int, stake: int, client_home: str = '.test_clif'):
     """
     Will deleted later
 
     nodef add-genesis-account <address> <initial_coin>,<initial_stake>
     """
 
-    _ = _process_executor("nodef add-genesis-account {} {}dummy,{}stake", address, coin, stake)
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    _ = _process_executor("nodef add-genesis-account {} {}dummy,{}stake --home-client {}", address, coin, stake, client_home)
 
 
-def add_el_genesis_account(address: str, coin: int, stake: int):
+def add_el_genesis_account(address: str, coin: int, stake: int, client_home: str = '.test_clif'):
     """
     nodef add-el-genesis-account <address> <initial_coin> <initial_stake>
     """
 
-    _ = _process_executor("nodef add-el-genesis-account {} {} {}", address, coin, stake)
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    _ = _process_executor("nodef add-el-genesis-account {} {} {} --home-client {}", address, coin, stake, client_home)
 
-def clif_configs(chain_id: str):
+def clif_configs(chain_id: str, client_home: str = '.test_clif'):
     """
     clif configs for easy use
     """
+    client_home = os.path.join(os.environ["HOME"], client_home)
     cmds = [
-        "clif config chain-id {}".format(chain_id),
-        "clif config output json",
-        "clif config trust-node true",
-        "clif config indent true"
+        "clif config chain-id {} --home {}".format(chain_id, client_home),
+        "clif config output json --home {}".format(client_home),
+        "clif config trust-node true --home {}".format(client_home),
+        "clif config indent true --home {}".format(client_home)
     ]
 
     for cmd in cmds:
@@ -198,12 +217,13 @@ def load_chainspec():
     _ = _process_executor(cmd, path)
     
 
-def gentx(wallet_alias: str, passphrase: str):
+def gentx(wallet_alias: str, passphrase: str, client_home: str = '.test_clif'):
     """
     nodef gentx --name <wallet_alias>
     """
+    client_home = os.path.join(os.environ["HOME"], client_home)
     try:
-        child = pexpect.spawn("nodef gentx --name {}".format(wallet_alias))
+        child = pexpect.spawn("nodef gentx --name {} --home-client {}".format(wallet_alias, client_home))
         _ = child.read_nonblocking(10000, timeout=1)
         _ = child.sendline(passphrase)
         
@@ -234,13 +254,14 @@ def unsafe_reset_all():
 
 
 def whole_cleanup():
-    for item in [[".nodef", "config"], [".nodef", "data"], [".clif"]]:
+    for item in [[".nodef", "config"], [".nodef", "data"], [".test_clif"]]:
         path = os.path.join(os.environ["HOME"], *item)
         shutil.rmtree(path, ignore_errors=True)
 
 
-def query_tx(tx_hash):
-    res = _process_executor("clif query tx {}", tx_hash, need_output=True)
+def query_tx(tx_hash, client_home: str = ".test_clif"):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    res = _process_executor("clif query tx {} --home {}", tx_hash, client_home, need_output=True)
     return res
 
 
@@ -262,16 +283,19 @@ def get_bls_pubkey_remote(remote_address):
 ## Nickname CLI
 #################
 
-def set_nickname(passphrase: str, nickname: str, address: str, node: str = "tcp://localhost:26657"):
-    return _tx_executor("clif nickname set {} --from {} --node {}", passphrase, nickname, address, node)
+def set_nickname(passphrase: str, nickname: str, address: str, node: str = "tcp://localhost:26657", client_home: str = '.test_clif'):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    return _tx_executor("clif nickname set {} --from {} --node {} --home {}", passphrase, nickname, address, node, client_home)
 
 
-def change_address_of_nickname(passphrase: str, nickname: str, new_address: str, old_address: str, node: str = "tcp://localhost:26657"):
-    return _tx_executor("clif nickname change-to {} {} --from {} --node {}", passphrase, nickname, new_address, old_address, node)
+def change_address_of_nickname(passphrase: str, nickname: str, new_address: str, old_address: str, node: str = "tcp://localhost:26657", client_home: str = '.test_clif'):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    return _tx_executor("clif nickname change-to {} {} --from {} --node {} --home {}", passphrase, nickname, new_address, old_address, node, client_home)
 
 
-def get_address(nickname: str, node: str = "tcp://localhost:26657"):
-    res = _process_executor("clif nickname get-address {} --node {}", nickname, node, need_output=True)
+def get_address(nickname: str, node: str = "tcp://localhost:26657", client_home: str = '.test_clif'):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    res = _process_executor("clif nickname get-address {} --node {} --home {}", nickname, node, client_home, need_output=True)
     return res
 
 
@@ -279,23 +303,28 @@ def get_address(nickname: str, node: str = "tcp://localhost:26657"):
 ## Hdac custom CLI
 ##################
 
-def transfer_to(passphrase: str, recipient: str, amount: int, fee: int, gas_price: int, from_value: str, node: str = "tcp://localhost:26657"):
-    return _tx_executor("clif hdac transfer-to {} {} {} {} --from {} --node {}", passphrase, recipient, amount, fee, gas_price, from_value, node)
+def transfer_to(passphrase: str, recipient: str, amount: int, fee: int, gas_price: int, from_value: str, node: str = "tcp://localhost:26657", client_home: str = '.test_clif'):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    return _tx_executor("clif hdac transfer-to {} {} {} {} --from {} --node {} --home {}", passphrase, recipient, amount, fee, gas_price, from_value, node, client_home)
 
 
-def bond(passphrase: str, amount: int, fee: int, gas_price: int, from_value: str, node: str = "tcp://localhost:26657"):
-    return _tx_executor("clif hdac bond {} {} {} --from {} --node {}", passphrase, amount, fee, gas_price, from_value, node)
+def bond(passphrase: str, amount: int, fee: int, gas_price: int, from_value: str, node: str = "tcp://localhost:26657", client_home: str = '.test_clif'):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    return _tx_executor("clif hdac bond {} {} {} --from {} --node {} --home {}", passphrase, amount, fee, gas_price, from_value, node, client_home)
 
 
-def unbond(passphrase: str, amount: int, fee: int, gas_price: int, from_value: str, node: str = "tcp://localhost:26657"):
-    return _tx_executor("clif hdac unbond {} {} {} --from {} --node {}", passphrase, amount, fee, gas_price, from_value, node)
+def unbond(passphrase: str, amount: int, fee: int, gas_price: int, from_value: str, node: str = "tcp://localhost:26657", client_home: str = '.test_clif'):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    return _tx_executor("clif hdac unbond {} {} {} --from {} --node {} --home {}", passphrase, amount, fee, gas_price, from_value, node, client_home)
 
 
-def get_balance(from_value: str, node: str = "tcp://localhost:26657"):
-    res = _process_executor("clif hdac getbalance --from {} --node {}", from_value, node, need_output=True)
+def get_balance(from_value: str, node: str = "tcp://localhost:26657", client_home: str = '.test_clif'):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    res = _process_executor("clif hdac getbalance --from {} --node {} --home {}", from_value, node, client_home, need_output=True)
     return res
 
 
-def create_validator(passphrase: str, from_value: str, pubkey: str, moniker: str, identity: str='""', website: str='""', details: str='""', node: str = "tcp://localhost:26657"):
-    return _tx_executor("clif hdac create-validator --from {} --pubkey {} --moniker {} --identity {} --website {} --details {} --node {}",
-                      passphrase, from_value, pubkey, moniker, identity, website, details, node)
+def create_validator(passphrase: str, from_value: str, pubkey: str, moniker: str, identity: str='""', website: str='""', details: str='""', node: str = "tcp://localhost:26657", client_home: str = '.test_clif'):
+    client_home = os.path.join(os.environ["HOME"], client_home)
+    return _tx_executor("clif hdac create-validator --from {} --pubkey {} --moniker {} --identity {} --website {} --details {} --node {} --home {}",
+                      passphrase, from_value, pubkey, moniker, identity, website, details, node, client_home)
