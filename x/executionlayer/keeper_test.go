e22a84afffffa66a142e670821a1269e782b85c3
Joowon <iam.yunjoowon@gmail.com>
2020-04-20 17:59:48 +0900

    refactor: using height instead of blockhash

--- x/executionlayer/keeper_test.go
+++ x/executionlayer/keeper_test.go
@@ -47,24 +47,22 @@ func TestQueryKeyToBytes(t *testing.T) {
 func TestUnitHashMapNormalInput(t *testing.T) {
 	input := setupTestInput()
 
-	blockHash := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}
 	eeState := []byte{31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-	result := input.elk.SetEEState(input.ctx, blockHash, eeState)
+	result := input.elk.SetUnitHashMap(input.ctx, NewUnitHashMap(eeState))
 	assert.Equal(t, true, result)
 
-	unitHash := input.elk.GetUnitHashMap(input.ctx, blockHash)
+	unitHash := input.elk.GetUnitHashMap(input.ctx, input.ctx.BlockHeight())
 	assert.Equal(t, eeState, unitHash.EEState)
 }
 
 func TestUnitHashMapInCorrectInput(t *testing.T) {
 	input := setupTestInput()
 
-	blockHash := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31}
 	eeState := []byte{31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
-	result := input.elk.SetEEState(input.ctx, blockHash, eeState)
+	result := input.elk.SetUnitHashMap(input.ctx, NewUnitHashMap(eeState))
 	assert.Equal(t, false, result)
 
-	unitHash := input.elk.GetUnitHashMap(input.ctx, blockHash)
+	unitHash := input.elk.GetUnitHashMap(input.ctx, input.ctx.BlockHeight())
 	assert.NotEqual(t, eeState, unitHash.EEState)
 }
 
