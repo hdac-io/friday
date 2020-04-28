bdddc881d225394c8c332dfe30d80ba5126a0c52
Jiyong Ha <slave5vw@gmail.com>
2020-04-28 17:34:26 +0900

    feat: attach to friday consensus
     * It’s connected to branch ‘features/friday_pipelining_production’ in hdac-io/tendermint repo.

--- server/start.go
+++ server/start.go
@@ -151,7 +151,7 @@ func startInProcess(ctx *Context, appCreator AppCreator) (*node.Node, error) {
 	// create & start tendermint node
 	tmNode, err := node.NewNode(
 		cfg,
-		pvm.LoadOrGenFilePV(cfg.PrivValidatorKeyFile(), cfg.PrivValidatorStateFile()),
+		pvm.LoadOrGenFridayFilePV(cfg.PrivValidatorKeyFile(), cfg.PrivValidatorStateFile()),
 		nodeKey,
 		proxy.NewLocalClientCreator(app),
 		node.DefaultGenesisDocProviderFunc(cfg),
