18492eeb05e5f493db70ed37a49b11071d1c8dd7
Jiyong Ha <slave5vw@gmail.com>
2020-04-28 16:11:50 +0900

    feat: change hardcoded keys for 32bytes key in test

--- x/executionlayer/types/types_test.go
+++ x/executionlayer/types/types_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestNewPublicKeyFromAddress(t *testing.T) {
 	// valid input
-	bech32ValAddr := "fridayvaloper15evpva2u57vv6l5czehyk69s0wnq9hrk4gqxv2"
+	bech32ValAddr := "fridayvaloper1gp2u22697kz6slwa25k2tkhz6st2l0zx3hkfc5wdlpjaauv5czsqcd3mya"
 	byteAddr, err := sdk.GetFromBech32(bech32ValAddr, "fridayvaloper")
 	require.Nil(t, err)
 
--- x/executionlayer/types/validator_test.go
+++ x/executionlayer/types/validator_test.go
@@ -8,11 +8,11 @@ import (
 )
 
 func TestValidatorTestEquivalent(t *testing.T) {
-	accAddr := "friday19rxdgfn3grqgwc6zhyeljmyas3tsawn6qe0quc"
+	accAddr := "friday1gp2u22697kz6slwa25k2tkhz6st2l0zx3hkfc5wdlpjaauv5czsq2dwu8m"
 	acc, _ := sdk.AccAddressFromBech32(accAddr)
 	valAddr := sdk.ValAddress(acc)
 
-	require.Equal(t, "fridayvaloper19rxdgfn3grqgwc6zhyeljmyas3tsawn64dsges", valAddr.String())
+	require.Equal(t, "fridayvaloper1gp2u22697kz6slwa25k2tkhz6st2l0zx3hkfc5wdlpjaauv5czsqcd3mya", valAddr.String())
 
 	consPubKey, _ := sdk.GetConsPubKeyBech32("fridayvalconspub16jrl8jvqq98x7jjxfcm8252pwd4nv6fetpzk6nzx2ddyc3fn0p2rz4mwf44nqjtfga5k5at4xad82sjhx9r9zdfcwuc5uvt90934jjr4d4xk242909rxks28v9erv3jvwfcx2wp4fe8h54fsddu9zar5v3tyknrs8pykk2mw2p29j4n6w455c7j2d3x4ykft9akx6s24gsu8ys2nvayrykqst965z")
 	val1 := NewValidator(acc, consPubKey, Description{}, "0")
