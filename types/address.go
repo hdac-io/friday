a808287f85db5ff84cf2da831ae443d1fa2340af
Jiyong Ha <slave5vw@gmail.com>
2020-05-03 22:45:33 +0900

    feat: change coin type to empty slot

--- types/address.go
+++ types/address.go
@@ -36,11 +36,11 @@ const (
 	Bech32MainPrefix = "friday"
 
 	// Atom in https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-	CoinType = 118
+	CoinType = 1217
 
 	// BIP44Prefix is the parts of the BIP44 HD path that are fixed by
 	// what we used during the fundraiser.
-	FullFundraiserPath = "44'/118'/0'/0/0"
+	FullFundraiserPath = "44'/1217'/0'/0/0"
 
 	// PrefixAccount is the prefix for account keys
 	PrefixAccount = "acc"
